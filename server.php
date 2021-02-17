a0d49a23fb6acbad3533953951f16b210ce27cd6
1967 Chevrolet Camaro SS <61229333+camaro-67@users.noreply.github.com>
2021-02-17 10:05:43 +0700

    Create server.php

---
+++ server.php
@@ -0,0 +1,12 @@
+<?php
+// Array with names
+
+// get the q parameter from URL
+
+$hint = "Message from server-side";
+
+
+
+// Output "no suggestion" if no hint was found or output correct values
+echo $hint === "" ? "no suggestion" : $hint;
+?>
